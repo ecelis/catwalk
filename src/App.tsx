c9d1ab13a3c4808f19a2a8fc702198d916f606e8
Ernesto Celis <ernesto@celisdelafuente.net>
2024-10-07 10:48:19 -0600

    WIP codesandbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import { Vex, Stave, StaveNote, Formatter } from "vexflow";
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navigation from './components/Navigation';
+
+const VF = Vex.Flow;
+console.log("VexFlow Build: " + JSON.stringify(VF.BUILD));
+const notes = [
+  new StaveNote({ keys: ["c/4"], duration: "q" }),
+  new StaveNote({ keys: ["d/4"], duration: "q" }),
+  new StaveNote({ keys: ["b/4"], duration: "qr" }),
+  new StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" }),
+];
+
+const stave = new Stave(10, 0, 190);
 
 function App() {
+
+  const vfRef = useRef(null);
+
+  useEffect(() => {
+    if(vfRef) {
+      console.log(vfRef)
+      // const div: string | HTMLCanvasElement | HTMLDivElement = document.getElementById("vf") as HTMLDivElement;
+      const renderer = new VF.Renderer(vfRef.current as unknown as HTMLDivElement, VF.Renderer.Backends.SVG);
+      renderer.resize(200, 200);
+      const context = renderer.getContext();
+      context.setFont("Arial", 10);
+
+      // Add a clef and time signature.
+stave.addClef("treble").addTimeSignature("4/4");
+
+// Connect it to the rendering context and draw!
+stave.setContext(context).draw();
+// Helper function to justify and draw a 4/4 voice.
+Formatter.FormatAndDraw(context, stave, notes);
+
+// TODO export
+// const doc = new jsPDF();
+// const svgElement = div.childNodes[0];
+// doc.svg(svgElement).then(() => doc.save("score.pdf"));
+
+// console.log("Saved score.pdf");
+    }
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Container fluid>
+  <Row>
+    <Col>
+      <Navigation />
+    </Col>
+  </Row>
+  <Row>
+    <Col>
+      <main><div id="vf" ref={vfRef}></div></main>
+    </Col>
+  </Row>
+</Container>
   );
 }
 
