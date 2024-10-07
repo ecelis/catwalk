5ff214f377c611c058b93de66c72c07561cae3ee
Ernesto Celis <ernesto@celisdelafuente.net>
2024-10-07 17:08:45 +0000

    chore(project): remove unused files and change port to 9090

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import { Vex, Stave, StaveNote, Formatter } from "vexflow";
-import './App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Navigation from './components/Navigation';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Navigation from "./components/Navigation";
 
 const VF = Vex.Flow;
 console.log("VexFlow Build: " + JSON.stringify(VF.BUILD));
@@ -18,47 +17,51 @@ const notes = [
 const stave = new Stave(10, 0, 190);
 
 function App() {
-
   const vfRef = useRef(null);
 
   useEffect(() => {
-    if(vfRef) {
-      console.log(vfRef)
+    if (vfRef) {
+      console.log(vfRef);
       // const div: string | HTMLCanvasElement | HTMLDivElement = document.getElementById("vf") as HTMLDivElement;
-      const renderer = new VF.Renderer(vfRef.current as unknown as HTMLDivElement, VF.Renderer.Backends.SVG);
+      const renderer = new VF.Renderer(
+        vfRef.current as unknown as HTMLDivElement,
+        VF.Renderer.Backends.SVG
+      );
       renderer.resize(200, 200);
       const context = renderer.getContext();
       context.setFont("Arial", 10);
 
       // Add a clef and time signature.
-stave.addClef("treble").addTimeSignature("4/4");
+      stave.addClef("treble").addTimeSignature("4/4");
 
-// Connect it to the rendering context and draw!
-stave.setContext(context).draw();
-// Helper function to justify and draw a 4/4 voice.
-Formatter.FormatAndDraw(context, stave, notes);
+      // Connect it to the rendering context and draw!
+      stave.setContext(context).draw();
+      // Helper function to justify and draw a 4/4 voice.
+      Formatter.FormatAndDraw(context, stave, notes);
 
-// TODO export
-// const doc = new jsPDF();
-// const svgElement = div.childNodes[0];
-// doc.svg(svgElement).then(() => doc.save("score.pdf"));
+      // TODO export
+      // const doc = new jsPDF();
+      // const svgElement = div.childNodes[0];
+      // doc.svg(svgElement).then(() => doc.save("score.pdf"));
 
-// console.log("Saved score.pdf");
+      // console.log("Saved score.pdf");
     }
-  }, [])
+  }, []);
   return (
-<Container fluid>
-  <Row>
-    <Col>
-      <Navigation />
-    </Col>
-  </Row>
-  <Row>
-    <Col>
-      <main><div id="vf" ref={vfRef}></div></main>
-    </Col>
-  </Row>
-</Container>
+    <Container fluid>
+      <Row>
+        <Col>
+          <Navigation />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <main>
+            <div id="vf" ref={vfRef}></div>
+          </main>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
